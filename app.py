7610f36f05060f73f1ce85dda5aaba0a88fbeec0
tiankong1428 <3166140298@qq.com>
2025-02-24 20:19:07 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/message', methods=['GET'])
-def get_message():
-    return "这是一个通过 GET 请求返回的消息。"
+def get_data():
 
+@app.route('/message', methods=['POST'])
+def post_message():
+    data = request.json  # 假设客户端发送的是 JSON 数据
+    id = data.get("id","")
+    tj = data.get("tj","")
+    cs = requests.get("https://service-5hxd8gip-1252368878.sh.apigw.tencentcs.com/release/query_all?id="+str(id)+"&cmd=2&platform=1011").text
+    my ="".join(re.findall(r'"rmb": (.*?),',cs))
+    return my
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
