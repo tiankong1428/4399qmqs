8faca0c76466f90254917ba40007e25c91ae02c7
tiankong1428 <3166140298@qq.com>
2025-02-24 23:42:29 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ app = Flask(__name__)
 @app.route('/message', methods=['POST'])
 def post_message():
     data = request.json  # 假设客户端发送的是 JSON 数据
-    id = data.get("id","")
-    tj = data.get("tj","")
-    le = data.get("len","")
+    id = data.get("id",0)
+    tj = data.get("tj",0)
+    le = data.get("len",0)
     cs = requests.get("https://service-5hxd8gip-1252368878.sh.apigw.tencentcs.com/release/query_all?id="+str(id)+"&cmd=2&platform=1011").text
     a = "".join(re.findall(r'"weaponIDList": (.*?), "',cs))
     ap = "".join(re.findall(r'"atlasPower": (.*?),',cs))
     data_list = ast.literal_eval(a)
 # 获取列表长度
     length = len(data_list)
-    if str(tj) == ap and str(le) == length:
+    if tj == int(ap) and le == int(length):
         rmb ="".join(re.findall(r'"rmb": (.*?),',cs))
         bt = "".join(re.findall(r'"birthTime": (.*?),',cs))
         pattern = r'"oldDanScore":\s*(\{[^{}]*\})'
@@ -31,7 +31,7 @@ def post_message():
         filtered_data = {key: int(value) for key, value in matches}
         my = "rmb="+rmb+"&bt="+bt+"&os="+filtered_data
     else:
-        my = "0"
+        my = "error"
     return my
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
