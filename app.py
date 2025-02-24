1f8a70afe30a5daa244c403cb6b0bfee6e71b054
tiankong1428 <3166140298@qq.com>
2025-02-24 23:31:05 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ def post_message():
     data = request.json  # 假设客户端发送的是 JSON 数据
     id = data.get("id","")
     tj = data.get("tj","")
-    len = data.get("len","")
+    le = data.get("len","")
     cs = requests.get("https://service-5hxd8gip-1252368878.sh.apigw.tencentcs.com/release/query_all?id="+str(id)+"&cmd=2&platform=1011").text
     a = "".join(re.findall(r'"weaponIDList": (.*?), "',cs))
     ap = "".join(re.findall(r'"atlasPower": (.*?),',cs))
     data_list = ast.literal_eval(a)
 # 获取列表长度
     length = len(data_list)
-    if str(tj) == ap and str(len) == length:
+    if str(tj) == ap and str(le) == length:
         rmb ="".join(re.findall(r'"rmb": (.*?),',cs))
         bt = "".join(re.findall(r'"birthTime": (.*?),',cs))
         pattern = r'"oldDanScore":\s*(\{[^{}]*\})'
