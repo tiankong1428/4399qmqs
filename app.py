26dec0747c5cd5682e14a399337fd9d9bc760629
tiankong1428 <3166140298@qq.com>
2025-02-25 13:44:14 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -6,14 +6,37 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+ip_last_request = {}
+
+# 限制时间间隔（1 小时）
+REQUEST_INTERVAL = timedelta(hours=1)
+
+@app.before_request
+def check_request_interval():
+    client_ip = request.remote_addr  # 获取客户端 IP
+    current_time = datetime.now()
+
+    # 检查 IP 是否已经记录过
+    if client_ip in ip_last_request:
+        last_request_time = ip_last_request[client_ip]
+        # 检查时间间隔
+        if current_time - last_request_time < REQUEST_INTERVAL:
+            return jsonify({"error": "Too many requests. Please try again later."}), 429  # 返回 429 状态码（请求过多）
+    ip_last_request[client_ip] = current_time
+    
 @app.route('/message', methods=['POST'])
 def post_message():
+    ban = open("ban.txt","r",encoding="utf-8").read()
     rz = open("rz.txt","a", encoding="utf-8")
+    r = open("rz.txt","r", encoding="utf-8").read()
     data = request.json  # 假设客户端发送的是 JSON 数据
     id = data.get("id",0)
     tj = data.get("tj",0)
     le = data.get("len",0)
     client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    if client_ip in ban:
+        return "ban"
+    
     utc_time = datetime.utcnow()  # 获取 UTC 时间
     china_time = utc_time + timedelta(hours=8)  # 手动调整为北京时间
     request_time = china_time.strftime('%Y-%m-%d %H:%M:%S')
